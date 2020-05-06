7dcce4bbff42ce7b83b40af61e58aa41cf133487
Nicholas Galante <ngalant2@binghamton.edu>
2020-05-06 11:41:28 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,7 @@
+def reverse_each_word(string)
+ array = string.split(" ")
+ return_array = []
+ array.collect { |string| return_array << string.reverse }
+ return_array.join(" ")
+end 
+#with collect 
